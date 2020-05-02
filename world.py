3edd315ed072792117386d2d7b2a56e431fce264
Naman Gupta <namanryan@gmail.com>
2020-05-02 19:25:39 -0400

    Updated world.py
    Added user choice start point and goal point

--- world.py
+++ world.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # function to generate map
-def createMap(world, scale=40):
+def createMap(world, start=(0, 0), goal=(9, 9), scale=40):
 
     # scaling up world
     height = world.shape[0]*scale
@@ -14,11 +14,13 @@ def createMap(world, scale=40):
     for _ in range(10):
         obs_coord = np.random.randint(10, size=2)
         world = cv2.rectangle(world, (obs_coord[0]*scale, obs_coord[1]*scale),
-                              (obs_coord[0]*scale + scale, obs_coord[1]*scale + scale), (0, 0, 0), -1)
+                              ((obs_coord[0]+1) * scale, (obs_coord[1]+1) * scale), (0, 0, 0), -1)
 
-    # create goal point and start point
-    world = cv2.rectangle(world, (10*scale, 10*scale), (9*scale, 9*scale), (0, 255, 0), -1)
-    world = cv2.rectangle(world, (0*scale, 0*scale), (1*scale, 1*scale), (0, 0, 255), -1)
+    # create goal point
+    world = cv2.rectangle(world, (goal[0]*scale, goal[1]*scale), ((goal[0]+1) * scale, (goal[1]+1) * scale), (0, 255, 0), -1)
+
+    # create start point
+    world = cv2.rectangle(world, (start[0]*scale, start[1]*scale), ((start[0]+1) * scale, (start[1]+1) * scale), (0, 0, 255), -1)
 
     # display world
     world = cv2.flip(world, -1)
@@ -32,7 +34,9 @@ if __name__ == '__main__':
     # creating empty world
     w = np.ones((10, 10, 3))
 
-    w = createMap(w, scale=40)
+    start_point = (0, 0)
+
+    w = createMap(w, start=start_point, scale=40)
 
     # display world
     cv2.imshow('World', w)
