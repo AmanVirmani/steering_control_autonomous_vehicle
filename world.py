e3f64e96b8018e20c342924b8d478b688ef94cc1
Naman Gupta <namanryan@gmail.com>
2020-05-02 19:07:22 -0400

    Renamed to world.py
    Completed world

--- world.py
+++ world.py
@@ -15,11 +15,15 @@ def createMap(world, scale=40):
         obs_coord = np.random.randint(10, size=2)
         world = cv2.rectangle(world, (obs_coord[0]*scale, obs_coord[1]*scale),
                               (obs_coord[0]*scale + scale, obs_coord[1]*scale + scale), (0, 0, 0), -1)
-        #cv2.re
+
+    # create goal point and start point
+    world = cv2.rectangle(world, (10*scale, 10*scale), (9*scale, 9*scale), (0, 255, 0), -1)
+    world = cv2.rectangle(world, (0*scale, 0*scale), (1*scale, 1*scale), (0, 0, 255), -1)
 
     # display world
-    cv2.imshow('world', world)
-    cv2.waitKey(0)
+    world = cv2.flip(world, -1)
+
+    return world
 
 
 # main function
@@ -28,4 +32,8 @@ if __name__ == '__main__':
     # creating empty world
     w = np.ones((10, 10, 3))
 
-    createMap(w, scale=40)
+    w = createMap(w, scale=40)
+
+    # display world
+    cv2.imshow('World', w)
+    cv2.waitKey(0)
